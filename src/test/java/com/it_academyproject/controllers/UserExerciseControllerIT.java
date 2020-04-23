23197a6d2ed76db88b29e96edc57bc9a3b80aaab
garciacarlos78 <carles.garcia4@hotmail.com>
2020-04-23 13:00:49 +0200

    Added UserExerciseControllerIT

---
+++ src/test/java/com/it_academyproject/controllers/UserExerciseControllerIT.java
@@ -0,0 +1,24 @@
+package com.it_academyproject.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserExerciseControllerIT {
+
+    @Test
+    void getExerciseStudentByItinerary() {
+    }
+
+    @Test
+    void getAllExerciseswithStudents() {
+    }
+
+    @Test
+    void getExercisesByStudentIdNew() {
+    }
+
+    @Test
+    void setUserExerciseStatusData() {
+    }
+}
